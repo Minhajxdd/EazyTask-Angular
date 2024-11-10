e9a1cfec8d65a8bf1f1f8663ef24b6702777da67
Mohammed Minhaj <nijulinux@gmail.com>
2024-11-10 21:45:47 +0530

    task complete added

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,24 @@
+import { Component, computed, EventEmitter, Input, input, Output, output } from '@angular/core';
+
+import { type User } from './user.model';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
+  @Output() select = new EventEmitter<string>();
+
+  get ImagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectedUser() {
+    this.select.emit(this.user.id);
+  }
+}
